b2016f9c23c863b3057d9e45bda963a334e11f3f
momoepro <supahmomo@gmail.com>
2015-10-26 20:01:39 +0000

    Commit 1
    Projecto criado com ligacao ao primavera

---
+++ CompanyDashboard/CompanyDashboard/Areas/Controllers/ControllersAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace CompanyDashboard.Areas.Controllers
+{
+    public class ControllersAreaRegistration : AreaRegistration
+    {
+        public override string AreaName
+        {
+            get
+            {
+                return "Controllers";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context)
+        {
+            context.MapRoute(
+                "Controllers_default",
+                "Controllers/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
